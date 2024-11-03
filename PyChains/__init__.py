42e5b5981708e01b0a5146ca97931e1d004562a9
Arthur Dewes <arthurgdewes@gmail.com>
2024-11-03 20:17:32 -0300

    python package

---
+++ PyChains/__init__.py
@@ -0,0 +1,13 @@
+from linkedList import selectionSort
+from linkedList import primeLL
+from linkedList import factLL
+from linkedList import ll2l
+from linkedList import l2ll
+from linkedList import head
+from linkedList import tail
+from linkedList import count
+from linkedList import add
+from linkedList import decinc
+
+__all__ = ["selectionSort","primeLL","factLL","ll2l","l2ll",
+    "head", "tail", "count", "add", "decinc"]
